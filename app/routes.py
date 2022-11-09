f6e36c42a4993d9a4b3051010ab5e7c631ace3a6
Thor <71967190+thormander@users.noreply.github.com>
2022-11-09 10:32:04 -0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app 
 from app.forms import SearchForm
-from app.orm import Analysis
+from app.orm import Analysis, User
+from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
 @app.route('/index')
@@ -12,6 +13,7 @@ def index():
 	return render_template('index.html',title='Home',user=user,stats=stats)
 
 @app.route('/search', methods=['GET','POST'])
+@login_required
 def search():
 	form = SearchForm()
 	if form.validate_on_submit():
@@ -21,3 +23,22 @@ def search():
 				row.setRC27()
 		return render_template('search.html',title='Results',form=form, stats=stats) #if valid, present results
 	return render_template('search.html',title='Search',form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
