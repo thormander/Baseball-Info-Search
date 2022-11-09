d03c6d7511343ceb228c9002f5c096216295baee
Thor <71967190+thormander@users.noreply.github.com>
2022-11-09 10:33:43 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
 login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, orm
