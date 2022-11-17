155852571f3bd26d89ecde59460c7ec4b8789c45
Thor <71967190+thormander@users.noreply.github.com>
2022-11-17 08:46:43 -0600

    added flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_login import LoginManager
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
